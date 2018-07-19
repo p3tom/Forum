29c76379c5f2e4c3f8eb31050061b3560601e320
p3tom <preethikamaraj5@gmail.com>
2018-07-19 15:57:46 +0200

    changed table name from reply to answer and also created followup table. also renamed reply_id to answer_id

--- Database/questiondb.sql
+++ Database/questiondb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2018 at 09:34 AM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Generation Time: Jul 19, 2018 at 03:45 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,12 +25,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `answer_id` int(11) NOT NULL,
+  `ques_id` int(11) DEFAULT NULL,
+  `user_id` varchar(128) NOT NULL,
+  `date` datetime NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`answer_id`, `ques_id`, `user_id`, `date`, `message`) VALUES
+(1, NULL, 'Anounymous', '2018-06-20 17:38:30', 'html is a hyper text markup language..\r\n\r\n\r\nhtml is a hyper text markup language..\r\n\r\n\r\n'),
+(10, NULL, 'Anounymous', '2018-06-20 18:27:48', 'sfjkkhsdakjhfskjadhfkja'),
+(11, NULL, 'Anounymous', '2018-06-20 18:31:16', 'bcshdfglIASMASKIUEWYIEWHdhjsfn\r\nsfsdkhfkjsd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `followup`
+--
+
+CREATE TABLE `followup` (
+  `followup_id` int(11) NOT NULL,
+  `answer_id` int(11) DEFAULT NULL,
+  `user_id` varchar(128) NOT NULL,
+  `date` datetime NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
 CREATE TABLE `login` (
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `first_name`, `last_name`, `email`, `password`) VALUES
+(24, 'John', 'Watson', 'armydoctor@gmail.com', '$2y$10$AW8Sa3jakg6a4Z3v0hJkx.2TmCQ4tEKmDbWxCmBJtWe5ElS6gSgE.'),
+(25, 'Sherlock', 'Holmes', 'baskerville@gmail.com', '$2y$10$5O57Wa3zLGBpsMzH/GltPOeOoK0ZqObBk6a1B72PL8VJ/lUXooZ7i'),
+(68, 'Hercule', 'Poirot', 'belgiandetective@perfection.com', '$2y$10$CgzBLPmoB6YJQC6DzFAVaeCtTl7g.i8vKeAIbyNqeMO/Vy3fLvSrS'),
+(77, 'Jane', 'Marple', 'stmarymead@gmail.com', '$2y$10$QkD0EQeuKBKH3kKrgKz.ruVJNEhbxdNcJTMHrVIEEg0CJmeQYkQyS'),
+(83, 'First', 'Last', 'test@email.com', '$2y$10$utINny8h.Cncisf8BM6B/.Qe/QYUsrssjryPCnj.6HLuO4Omw7h8m'),
+(84, 'First', 'Last', 'test1@email.com', '$2y$10$JEF.UtG/sjT3v0Si7Bgp7e40npE3AOO2ULRU1NUhu4D66Geaqpz42'),
+(85, 'First', 'Last', 'test2@email.com', '$2y$10$cxw/pOOkv2CYKhmuu68Mb.j6V.cKUl9Kb5ylbLW11O1bTq3J4X96O'),
+(86, 'First', 'Last', 'test3@email.com', '$2y$10$U5FbXTMoBLngUBgRUi3/IO04impI8yfXMhUNDah2zKuKWbyjv8Gou'),
+(87, 'First', 'Last', 'test4@email.com', '$2y$10$fDkbdj1XboWXbOmyge6PvOGPMJdZbLXoQqpEOS2UlXr96SvjwxK.O'),
+(88, 'First', 'Last', 'test5@email.com', '$2y$10$NDSEXx/jeCZ2QAqKQZ.S/ebgDwI1dpyESjWVojOpXHRY/hGVYbZHK');
 
 -- --------------------------------------------------------
 
@@ -45,23 +102,33 @@ CREATE TABLE `question` (
   `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `reply`
+-- Dumping data for table `question`
 --
 
-CREATE TABLE `reply` (
-  `reply_id` int(11) NOT NULL,
-  `ques_id` int(11) NOT NULL,
-  `user_id` varchar(128) NOT NULL,
-  `date` datetime NOT NULL,
-  `message` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `question` (`ques_id`, `user_id`, `date`, `message`) VALUES
+(2, 'Anounymous', '2018-06-20 17:37:37', 'What is HTML? '),
+(3, 'Anounymous', '2018-06-20 17:38:05', 'What is CSS?'),
+(4, 'Anounymous', '2018-07-03 21:54:19', 'What is Jquery?'),
+(5, 'Anounymous', '2018-07-03 21:58:28', 'What is PHP?');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `answer`
+--
+ALTER TABLE `answer`
+  ADD PRIMARY KEY (`answer_id`),
+  ADD KEY `ques_id` (`ques_id`);
+
+--
+-- Indexes for table `followup`
+--
+ALTER TABLE `followup`
+  ADD PRIMARY KEY (`followup_id`),
+  ADD KEY `answer_id` (`answer_id`);
 
 --
 -- Indexes for table `login`
@@ -76,32 +143,42 @@ ALTER TABLE `question`
   ADD PRIMARY KEY (`ques_id`);
 
 --
--- Indexes for table `reply`
---
-ALTER TABLE `reply`
-  ADD PRIMARY KEY (`reply_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `followup`
+--
+ALTER TABLE `followup`
+  MODIFY `followup_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `ques_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ques_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `reply`
+-- Constraints for dumped tables
 --
-ALTER TABLE `reply`
-  MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for table `answer`
+--
+ALTER TABLE `answer`
+  ADD CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`ques_id`) REFERENCES `question` (`ques_id`);
+
+--
+-- Constraints for table `followup`
+--
+ALTER TABLE `followup`
+  ADD CONSTRAINT `followup_ibfk_1` FOREIGN KEY (`answer_id`) REFERENCES `answer` (`answer_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
