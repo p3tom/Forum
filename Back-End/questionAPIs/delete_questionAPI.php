c7071394b8188c9fec1404003993c7092cc14ba7
PriyankaRoy5 <40205521+PriyankaRoy5@users.noreply.github.com>
2018-07-24 10:49:01 -0400

    minor changes to questionAPIs for testing

--- Back-End/questionAPIs/delete_questionAPI.php
+++ Back-End/questionAPIs/delete_questionAPI.php
@@ -10,9 +10,10 @@ session_start();
             $sql = "DELETE FROM question WHERE ques_id='$ques_id'";
                 $result = mysqli_query($connection, $sql);
                 $json_array = array();
-                
+
                 while($row = mysqli_fetch_assoc($result)){
                     $json_array[] = $row;
                 }
                     echo json_encode($json_array);
    // }
+?>
--- Back-End/questionAPIs/edit_questionAPI.php
+++ Back-End/questionAPIs/edit_questionAPI.php
@@ -6,25 +6,16 @@
     //$user_id = $_SESSION['email'];
     $date = time();
     $message = $_POST['message'];
-<<<<<<< HEAD:Back-End/question APIs/edit_questionAPI.php
-    $connection = mysqli_connect("localhost", "root", "", "questiondb"); 
+    $connection = mysqli_connect("localhost", "root", "", "questiondb");
     $update_quesquery = "UPDATE question SET message='$message', date='$date' WHERE ques_id='$ques_id'";
     $result = mysqli_query($connection, $update_quesquery);
-=======
     $connection = mysqli_connect("localhost", "root", "", "questiondb");
     $update_query = "UPDATE question SET message='$message', post_date='$date'  WHERE ques_id='$ques_id'";
     $result = mysqli_query($connection, $update_query);
->>>>>>> 037edb202c798bf4096534dd8d08ad5e33417621:Back-End/edit_questionAPI.php
     $json_array = array();
     while($row = mysqli_fetch_assoc($result)){
       $json_array[] = $row;
     }
     echo json_encode($json_array);
-<<<<<<< HEAD:Back-End/question APIs/edit_questionAPI.php
-  }
-  
 
-
-=======
   //}
->>>>>>> 037edb202c798bf4096534dd8d08ad5e33417621:Back-End/edit_questionAPI.php
