f8f05337b306476b2f57f524fc5356045c2e1097
p3tom <preethikamaraj5@gmail.com>
2018-07-27 11:54:14 +0200

    small changes in add and edit question APIs

--- Back-End/questionAPIs/edit_questionAPI.php
+++ Back-End/questionAPIs/edit_questionAPI.php
@@ -7,7 +7,7 @@
     $is_filled = true;
     $ques_id = $_POST['ques_id'];
     //$user_id = $_SESSION['email'];
-    $date = time();
+    $date = date('Y-m-d H:i:s', time());
     $message = $_POST['message'];
     foreach($inputArray as $row => $postRow){ #make array of inputs
       foreach($_POST as $postRow => $value){
@@ -35,17 +35,17 @@
         $update_quesquery = "UPDATE question SET message='$message', post_date='$date'  WHERE ques_id= '$ques_id'";
         $result = mysqli_query($connection, $update_quesquery);
 
-        $update_quesquery1 =  "SELECT * from question where ques_id = '$ques_id'";
-        $result1 = mysqli_query($connection, $update_quesquery1);
-        $json_array = array();
+       // $update_quesquery1 =  "SELECT * from question where ques_id = '$ques_id'";
+        //$result1 = mysqli_query($connection, $update_quesquery1);
+        //$json_array = array();
         //print_r($result1) ;
-        while($row = mysqli_fetch_assoc($result1)){
+        /*while($row = mysqli_fetch_assoc($result1)){
           $json_array[] = $row;
         }
         echo json_encode($json_array);
         $question_Data = ['Question ID'=>$ques_id, 'Date'=>$date, 'Message'=> $message];
         $json = json_encode($question_Data);
-      //  echo $json;
+      //  echo $json;*/
       echo $json = '1';
       }
     }
